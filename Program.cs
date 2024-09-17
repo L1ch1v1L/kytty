800a6ef42dd1b639eee48b70df5d1831a8d76758
L1ch1v1L <69520996+L1ch1v1L@users.noreply.github.com>
2024-09-17 10:21:56 +0500

    перепутал код

--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
-﻿using System;
-namespace fds
+using System;
+namespace fds;
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        // Ввод пользователя
+        Console.Write("Введите первый элемент (a1): ");
+        double a1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите разность прогрессии (d): ");
+        double d = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите количество элементов (n): ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        // Вычисление и вывод элементов прогрессии
+        Console.WriteLine("Элементы арифметической прогрессии:");
+
+        for (int i = 0; i < n; i++)
         {
-            string str = Console.ReadLine();
-            bool b = true;
-            for (int i = 0; i < str.Length / 2; i++)
-            {
-                if (str[i] != str[str.Length - i - 1]) { b = false; }
-            }
-            Console.WriteLine(b);
-            Console.ReadKey();
+            double an = a1 + i * d; // Формула для нахождения n-го элемента
+            Console.WriteLine($"a{i + 1} = {an}");
         }
     }
 }
